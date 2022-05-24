64ca0c174a2cf5882ab25eb81a30393eef506f65
seoye0ng <101791501+seoye0ng@users.noreply.github.com>
2022-05-24 21:17:41 +0900

    4_Loading_To_Dos_part_One

--- js/todo.js
+++ js/todo.js
@@ -3,10 +3,12 @@ const toDoInput = document.querySelector("#todo-form input");
 //const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+
 const toDos = [];
 
 function saveToDos() {
-  localStorage.setItem("todos", JSON.stringify(toDos));
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function deleteToDo(event) {
@@ -36,3 +38,10 @@ function handleToDoSubmit(event) {
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  parsedToDos.forEach((item) => console.log("this is the turn of", item));
+}
